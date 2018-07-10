ce677c10dbbbe010c74ec81ac234df99aa2b6839
David McCormick <dave.a.mccormick@gmail.com>
2018-07-10 17:22:35 +1000

    Improve some settings CSS

--- src/react/ViewerControls/settings.jsx
+++ src/react/ViewerControls/settings.jsx
@@ -4,6 +4,7 @@ export default class Settings extends Component {
     render() {
         return (
             <div className="settingsContainer">
+                <label className="setting-label">Models:</label>
                 <div className="settingsModelsContainer">
                     <div className="models">
                     </div>
@@ -11,12 +12,14 @@ export default class Settings extends Component {
                         <button className="primary add-model">Add</button>
                     </div>
                 </div>
+                <label className="setting-label">Capture:</label>
                 <div className="segmented">
                     <label className="checked option-live"><input type="radio" name="segmented" defaultChecked={true} />Live</label>
                     <label className="option-video"><input type="radio" name="segmented" />Video</label>
                     <label className="option-image"><input type="radio" name="segmented" />Image</label>
                 </div>
                 <div className='busyMode'>
+                    <label className="setting-label">Logs:</label>
                     <div className='field'>
                         <label className='label label-toggle' htmlFor='night-mode2'>
                         <div className='input-toggle'>
