7aad392e6b6f198254357707bd1ab222242ed3aa
David McCormick <dave.a.mccormick@gmail.com>
2018-07-12 18:58:37 +1000

    Fix other stuff too

--- src/containers/pimpedmenu.jsx
+++ src/containers/pimpedmenu.jsx
@@ -29,7 +29,7 @@ class PimpedMenu extends Component {
                     </header>
                     <section className="sections">
                         <Welcome cssClass={this.showHideSection(Globals.sectionIDs.welcome)} />
-                        <div id={Globals.sectionIDs.about} className={this.showHideSection(Globals.sectionIDs.about)}>
+                        <div id={Globals.sectionIDs.demo} className={this.showHideSection(Globals.sectionIDs.demo)}>
                             <App />
                         </div>
                         <div id={Globals.sectionIDs.contact} className={this.showHideSection(Globals.sectionIDs.contact)}>
