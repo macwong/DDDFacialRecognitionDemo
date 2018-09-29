40a73d267f21a08c8cb0af23cc7aa2e357091b7c
David McCormick <dmccormick@starrez.com>
2018-09-29 15:59:59 +1000

    Add info section

--- src/components/slidersection.jsx
+++ src/components/slidersection.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
+import Helpers from "../js/helpers";
+import Globals from "../js/globals";
+import ModelInfoBlock from '../react/InfoBlocks/modelinfoblock';
 
 export default class SliderSection extends Component {
     constructor(props) {
@@ -55,56 +58,79 @@ export default class SliderSection extends Component {
             slidesToScroll: 1
         };
 
+        const pred = this.props.prediction;
+        const model_info = pred.model_info;
+
         return (
             <div id={this.props.section} className={"slider-section " + this.props.cssClass}>
-            <div>
-                <Slider {...settings}>
-                    <div className="box">
-                        Summary
-                    </div>
-                    <div className="box">
-                        Input: Original snapshot image
-                    </div>
-                    <div className="box">
-                        Find Face: Cropped snapshot
-                    </div>
-                    <div className="box">
-                        Convert: Embeddings
-                    </div>
-                    <div className="box">
-                        Predict: Final result
-                    </div>
-                    
-                    {/* <div>
+                <div>
+                    <Slider {...settings}>
                         <div className="box">
-                            <div className="row header">
-                                <p><b>header</b>
-                                <br />
-                                <br />(sized to content)</p>
+                            <figure className="profile">
+                                <img className="profile-pic" src={Globals.pngSource + pred.image} />
+                                <figcaption className="caption">
+                                    <h2>{pred.pred_name}</h2>
+                                </figcaption>
+                                <img className="icon" src={Helpers.getPredictionIcon(Helpers.getIndividualPredictionInfo(pred.pred_info, pred.pred_name).distance)} />
+                            </figure>
+                            <div className="scores">
+                                <div className="Rtable Rtable--2cols Rtable--collapse">
+                                    <div className="table-header Rtable-cell Rtable-cell--alignCenter"><h3>Probability</h3></div>
+                                    <div className="table-cell probability Rtable-cell Rtable-cell--alignCenter">
+                                    {Helpers.getProbability(pred.probability)}
+                                    </div>
+
+                                    <div className="table-header Rtable-cell Rtable-cell--alignCenter"><h3>Distance</h3></div>
+                                    <div className="table-cell distance Rtable-cell Rtable-cell--alignCenter">
+                                {pred.distance.toFixed(2)}
+                                    </div>
+                                </div>
                             </div>
-                            <div className="row content">
-                                <p>
-                                <b>content</b>
-                                (fills remaining space)
-                                </p>
-                            </div>
-                            <div className="row footer">
-                                <p><b>footer</b> (fixed height)</p>
+                            <ModelInfoBlock model_info={model_info} />
+                        </div>
+                        <div className="box">
+                            Input: Original snapshot image
+                        </div>
+                        <div className="box">
+                            Find Face: Cropped snapshot
+                        </div>
+                        <div className="box">
+                            Convert: Embeddings
+                        </div>
+                        <div className="box">
+                            Predict: Final result
+                        </div>
+                        
+                        {/* <div>
+                            <div className="box">
+                                <div className="row header">
+                                    <p><b>header</b>
+                                    <br />
+                                    <br />(sized to content)</p>
+                                </div>
+                                <div className="row content">
+                                    <p>
+                                    <b>content</b>
+                                    (fills remaining space)
+                                    </p>
+                                </div>
+                                <div className="row footer">
+                                    <p><b>footer</b> (fixed height)</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div>
-                        Test 2
-                    </div>
-                    <div>
-                        Test 3
-                    </div>
-                    <div>
-                        Test 4
-                    </div> */}
-                </Slider>
+                        <div>
+                            Test 2
+                        </div>
+                        <div>
+                            Test 3
+                        </div>
+                        <div>
+                            Test 4
+                        </div> */}
+                    </Slider>
+                </div>
             </div>
-        </div>
 
         );
     }
