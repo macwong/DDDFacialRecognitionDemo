a5cc5465917f703970f712e09568ca3e4825e356
David McCormick <dmccormick@starrez.com>
2018-09-30 09:43:05 +1000

    Remove unneeded code

--- src/components/slidersection.jsx
+++ src/components/slidersection.jsx
@@ -114,34 +114,6 @@ export default class SliderSection extends Component {
                         <div className="box">
                             Predict: Final result
                         </div>
-                        
-                        {/* <div>
-                            <div className="box">
-                                <div className="row header">
-                                    <p><b>header</b>
-                                    <br />
-                                    <br />(sized to content)</p>
-                                </div>
-                                <div className="row content">
-                                    <p>
-                                    <b>content</b>
-                                    (fills remaining space)
-                                    </p>
-                                </div>
-                                <div className="row footer">
-                                    <p><b>footer</b> (fixed height)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            Test 2
-                        </div>
-                        <div>
-                            Test 3
-                        </div>
-                        <div>
-                            Test 4
-                        </div> */}
                     </Slider>
                 </div>
             </div>
