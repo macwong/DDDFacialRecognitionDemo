5b30955f9131009e566c554538a234b238c1a431
David McCormick <dmccormick@starrez.com>
2018-10-06 10:02:21 +1000

    Intro

--- src/components/slides/01 - intro.jsx
+++ src/components/slides/01 - intro.jsx
@@ -10,40 +10,25 @@ export default class IntroSlides extends Component {
     render() {
         return (
             <PowerpointSection 
-            section={this.props.section} 
-            cssClass={this.props.cssClass} 
-        >
-            <Slide>
-                <div className="row header">
-                    <h1>Header</h1>
-                </div>
-                <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
-                </div>
-                <div className="row footer">
-                    Footer
-                </div>
-            </Slide>
-            <Slide>
-                <div className="row header">
-                    <h1>Header</h1>
-                </div>
-                <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
-                </div>
-                <div className="row footer">
-                </div>
-            </Slide>
-        </PowerpointSection>
-
+                section={this.props.section} 
+                cssClass={this.props.cssClass} 
+            >
+                <Slide>
+                    <div className="row header">
+                        <h1>Facial Recognition</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
+                            <li>Basic concepts and features</li>
+                            <li>Pipeline</li>
+                            <li>Potential issues</li>
+                            <li>Technologies and resources</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+            </PowerpointSection>
         );
     }
 }
