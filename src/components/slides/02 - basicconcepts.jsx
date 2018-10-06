5bc3dc369c90f691303646901e05d641f6f4e70b
David McCormick <dmccormick@starrez.com>
2018-10-07 09:31:12 +1100

    More slides

--- src/components/slides/02 - basicconcepts.jsx
+++ src/components/slides/02 - basicconcepts.jsx
@@ -46,13 +46,33 @@ export default class BasicConceptsSlides extends Component {
                 </Slide>
                 <Slide>
                     <div className="row header">
-                        <h1>Features</h1>
+                        <h1>APIs</h1>
                     </div>
                     <div className="row content">
                         <ul>
-                            <li>First point</li>
-                            <li>Second point</li>
-                            <li>Third point</li>
+                            <li>Microsoft: Face API</li>
+                            <li>Amazon: Rekognition</li>
+                            <li>Google: Cloud Vision</li>
+                            <li>Roll your own?</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>Features of Facial Recognition System</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
+                            <li>Predictions from image, video and cam</li>
+                            <li>Real-time predictions</li>
+                            <li>Can predict from a "large" dataset of faces</li>
+                            <li>Robust to variables (e.g. facial expressions, lighting conditions, occlusions)</li>
+                            <li>Measure accuracy of predictions</li>
+                            <li>Identify closest matches (e.g. Top 5)</li>
+                            <li>"Train" and choose different "databases" to find person</li>
+                            <li>Add new people to system</li>
                         </ul>
                     </div>
                     <div className="row footer">
