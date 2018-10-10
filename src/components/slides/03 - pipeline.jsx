fec07349213f709348cde18d146bf38998da3ace
David McCormick <dmccormick@starrez.com>
2018-10-11 00:49:26 +1100

    Some pipeline slides

--- src/components/slides/03 - pipeline.jsx
+++ src/components/slides/03 - pipeline.jsx
@@ -15,24 +15,47 @@ export default class PipelineSlides extends Component {
         >
             <Slide>
                 <div className="row header">
-                    <h1>Pipeline - Demo</h1>
+                    <h1>How does it work?</h1>
                 </div>
                 <div className="row content">
-                    Demo...
+                    <div style={{width: '100%'}}>
+                        <ul>
+                            <li>What is going on between the input and the output?</li>
+                        </ul>
+                        <div className="centre-image" style={{height: '400px'}}>
+                        <img src="./images/question.png" />
+                        </div>
+                    </div>
                 </div>
                 <div className="row footer">
                 </div>
             </Slide>
             <Slide>
                 <div className="row header">
-                    <h1>1. Initial Input</h1>
+                    <h1>Pipeline Overview</h1>
                 </div>
                 <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
+                    <div className="centre-image" style={{height: '400px', width: '100%'}}>
+                        <img src="./images/pipeline.png" />
+                    </div>
+                </div>
+                <div className="row footer">
+                </div>
+            </Slide>
+            <Slide>
+                <div className="row header">
+                    <h1>1. Input</h1>
+                </div>
+                <div className="row content">
+                    <div style={{width: '100%'}}>
+                        <div className="centre-image" style={{marginBottom: '10px'}}>
+                            <img src="./images/1 - input.png" />
+                        </div>
+                        <ul>
+                            <li>Images, video or live camera</li>
+                            <li>Can be variable in resolution and quality</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="row footer">
                 </div>
@@ -42,11 +65,15 @@ export default class PipelineSlides extends Component {
                     <h1>2. Face Detection</h1>
                 </div>
                 <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
+                    <div style={{width: '100%'}}>
+                        <div className="centre-image" style={{marginBottom: '10px'}}>
+                            <img src="./images/2 - face detection.png" />
+                        </div>
+                        <ul>
+                            <li>Images, video or live camera</li>
+                            <li>Can be variable in resolution and quality</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="row footer">
                 </div>
@@ -56,11 +83,15 @@ export default class PipelineSlides extends Component {
                     <h1>3. FaceNet Conversion</h1>
                 </div>
                 <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
+                    <div style={{width: '100%'}}>
+                        <div className="centre-image" style={{marginBottom: '10px'}}>
+                            <img src="./images/3 - conversion.png" />
+                        </div>
+                        <ul>
+                            <li>Images, video or live camera</li>
+                            <li>Can be variable in resolution and quality</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="row footer">
                 </div>
@@ -70,11 +101,33 @@ export default class PipelineSlides extends Component {
                     <h1>4. Prediction</h1>
                 </div>
                 <div className="row content">
-                    <ul>
-                        <li>First point</li>
-                        <li>Second point</li>
-                        <li>Third point</li>
-                    </ul>
+                    <div style={{width: '100%'}}>
+                        <div className="centre-image" style={{marginBottom: '10px'}}>
+                            <img src="./images/4 - prediction.png" />
+                        </div>
+                        <ul>
+                            <li>Images, video or live camera</li>
+                            <li>Can be variable in resolution and quality</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row footer">
+                </div>
+            </Slide>
+            <Slide>
+                <div className="row header">
+                    <h1>5. Output</h1>
+                </div>
+                <div className="row content">
+                    <div style={{width: '100%'}}>
+                        <div className="centre-image" style={{marginBottom: '10px'}}>
+                            <img src="./images/5 - output.png" />
+                        </div>
+                        <ul>
+                            <li>Images, video or live camera</li>
+                            <li>Can be variable in resolution and quality</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="row footer">
                 </div>
