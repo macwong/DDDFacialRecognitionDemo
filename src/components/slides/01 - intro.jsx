50f8810ff4a8e9ac665276ba0a0e55e42e93f3c0
Dave McCormick <dave.a.mccormick@gmail.com>
2019-08-02 17:07:07 +1000

    Intro skeleton

--- src/components/slides/01 - intro.jsx
+++ src/components/slides/01 - intro.jsx
@@ -15,15 +15,75 @@ export default class IntroSlides extends Component {
             >
                 <Slide>
                     <div className="row header">
-                        <h1>Facial Recognition</h1>
+                        <h1>What is Facial Recognition?</h1>
+                    </div>
+                    <div className="row content">
+                        <div style={{ width: '100%' }}>
+                            <ul>
+                                <li>Identifying a person from an image (or video frame)</li>
+                            </ul>
+                            <div className="centre-image" style={{ height: '400px' }}>
+                                <img src="./images/facerec.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>How does it work?</h1>
                     </div>
                     <div className="row content">
                         <ul>
                             <li>How does it work?</li>
-                            <li>Training</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>Accuracy</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
                             <li>Accuracy</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>Performance</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
                             <li>Performance</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>Technologies</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
                             <li>Technologies</li>
+                        </ul>
+                    </div>
+                    <div className="row footer">
+                    </div>
+                </Slide>
+                <Slide>
+                    <div className="row header">
+                        <h1>End-to-end Scenario</h1>
+                    </div>
+                    <div className="row content">
+                        <ul>
                             <li>End-to-end Scenario</li>
                         </ul>
                     </div>
